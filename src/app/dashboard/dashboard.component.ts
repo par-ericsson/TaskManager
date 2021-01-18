b241eea6a2e33f98e8ed933fa907b69c6c1e4760
Pär Ericsson <ericsson@home.se>
2021-01-18 20:47:26 +0100

    Replaced hardcoded code with ngFor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,6 +16,10 @@ export class DashboardComponent implements OnInit {
     this.ProjectCost = 0;
     this.CurrentExpenditure = 0;
     this.AvailableFunds = 0;
+    this.Clients = [];
+    this.Projects = [];
+    this.TeamMembersSummary = [];
+    this.TeamMembers = [];
   }
   Designation: string;
   Username: string;
@@ -27,6 +31,12 @@ export class DashboardComponent implements OnInit {
   CurrentExpenditure: number;
   AvailableFunds: number;
 
+  Clients: string[];
+  Projects: string[];
+  Years: number[] = [];
+  TeamMembersSummary: any = [];
+  TeamMembers: any = [];
+
   ngOnInit(): void {
     this.Designation = 'Team Leader';
     this.Username = 'John Doe';
@@ -37,6 +47,60 @@ export class DashboardComponent implements OnInit {
     this.ProjectCost = 2113507;
     this.CurrentExpenditure = 96788;
     this.AvailableFunds = 52536;
+
+    this.Clients = [
+      "Infotech Ltd.", "Acme Software Solutions", "Eidelund Industries"
+    ];
+
+    this.Projects = [
+      "Project A", "Project B", "Project C", "Project D"
+    ];
+
+    for(let i = 2021; i >= 2011; i--){
+      this.Years.push(i);
+    }
+
+    this.TeamMembersSummary = [
+      { Region: "East", TeamMembersCount: 20, TemporarilyUnavailableMembers: 4},
+      { Region: "West", TeamMembersCount: 25, TemporarilyUnavailableMembers: 8},
+      { Region: "South", TeamMembersCount: 17, TemporarilyUnavailableMembers: 1},
+      { Region: "North", TeamMembersCount: 15, TemporarilyUnavailableMembers: 6}
+    ];
+
+    this.TeamMembers = [
+      {
+        Region: "East", Members: [
+          {ID: 1, Name: "Ford", Status: "Available"},
+          {ID: 2, Name: "Miller", Status: "Available"},
+          {ID: 3, Name: "Jones", Status: "Busy"},
+          {ID: 4, Name: "James", Status: "Busy"}
+        ]
+      },
+      {
+        Region: "West", Members: [
+          {ID: 5, Name: "Anna", Status: "Available"},
+          {ID: 6, Name: "Elin", Status: "Available"},
+          {ID: 7, Name: "Jonas", Status: "Busy"},
+          {ID: 8, Name: "Joe", Status: "Busy"}
+        ]
+      },
+      {
+        Region: "South", Members: [
+          {ID: 9, Name: "Anders", Status: "Available"},
+          {ID: 10, Name: "Joakim", Status: "Available"},
+          {ID: 11, Name: "Sharon", Status: "Busy"},
+          {ID: 12, Name: "David", Status: "Busy"}
+        ]
+      },
+      {
+        Region: "North", Members: [
+          {ID: 13, Name: "Susan", Status: "Available"},
+          {ID: 14, Name: "Tim", Status: "Available"},
+          {ID: 15, Name: "Jennie", Status: "Busy"},
+          {ID: 16, Name: "Alexandra", Status: "Busy"}
+        ]
+      },
+    ];
   }
 
 }
